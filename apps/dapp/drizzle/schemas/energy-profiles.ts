58826db6037e4459510c41fa5164234a326e0d39
doinel1a <doinel1atanasiu@gmail.com>
2025-10-21 13:09:05 +0200

    fix(drizzle): relation between users & energy profiles schemas

--- apps/dapp/drizzle/schemas/energy-profiles.ts
+++ apps/dapp/drizzle/schemas/energy-profiles.ts
@@ -1,16 +1,10 @@
 import { index, integer, pgTable, timestamp, varchar } from 'drizzle-orm/pg-core';
 
-import { usersSchema } from './users';
-
 export const energyProfilesSchema = pgTable(
   'energy_profiles',
   {
     id: integer().primaryKey().generatedAlwaysAsIdentity(),
-    userId: integer('user_id')
-      .notNull()
-      .unique()
-      .references(() => usersSchema.id, { onDelete: 'cascade' }),
-    role: varchar({ length: 50 }).notNull(),
+    role: varchar({ length: 50 }).notNull().unique(),
 
     createdAt: timestamp('created_at', { mode: 'date', precision: 3 }).notNull().defaultNow(),
     updatedAt: timestamp('updated_at', { mode: 'date', precision: 3 }).$onUpdateFn(() => new Date())
--- apps/dapp/drizzle/schemas/users.ts
+++ apps/dapp/drizzle/schemas/users.ts
@@ -1,8 +1,14 @@
 import { integer, pgTable, timestamp, varchar } from 'drizzle-orm/pg-core';
 
+import { energyProfilesSchema } from './energy-profiles';
+
 export const usersSchema = pgTable('users', {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   address: varchar({ length: 255 }).notNull().unique(),
+  energyProfileId: integer('energy_profile_id')
+    .notNull()
+    .references(() => energyProfilesSchema.id, { onDelete: 'restrict' }),
+
   createdAt: timestamp('created_at', { mode: 'date', precision: 3 }).notNull().defaultNow(),
   updatedAt: timestamp('updated_at', { mode: 'date', precision: 3 }).$onUpdateFn(() => new Date())
 });
